c012d1969dcda4972d145fe244a0a92d6ce6b611
cahves1 <athoschaveslopesdealmeida@gmail.com>
2025-05-21 19:49:57 -0300

    Condições em JS

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 60.8
+
+if(vel > 60){
+  console.log(`A velocidade do seu carro é ${vel}Km/h.`)
+  console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+} else {
+  console.log(`Dirija com segurança.`)
+}
---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+var país = 'Brasil';
+
+console.log(`Nascido em ${país}`)
+if(país == 'Brasil' || país == 'brasil') {
+    console.log('Você é brasileiro')
+} else{
+    console.log('Você é estrangeiro')
+}
