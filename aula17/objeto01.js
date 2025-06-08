ea91a4e36df32ecdf8542158b5694e96765ddcd0
cahves1 <athoschaveslopesdealmeida@gmail.com>
2025-06-08 20:44:38 -0300

    objetos

--- aula17/objeto01.js
+++ aula17/objeto01.js
@@ -1,8 +1,9 @@
-let amigo = {nome: 'José', sexo: 'M', peso: '85.4', 
+let amigo = {nome: 'José', sexo: 'M', peso: 85.4, 
 engordar(p=0) {
   console.log('Engordou')
   this.peso += p
 }
 }
 
-console.log(`${amigo.nome} pesa ${amigo.peso}`)
+amigo.engordar(2)
+console.log(`${amigo.nome} pesa ${amigo.peso}Kg`)
